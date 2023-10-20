27d2f52be83d833311110ccf1edd2d29001a966c
Bülent ER <bropil@gmail.com>
2023-10-20 23:36:46 +0300

    comments can be edited

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,16 @@ class CommentsController < ApplicationController
       render 'posts/show'
     end
   end
+  def update
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_url(@post), notice: "Comment was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entit, alert: "Comment was not  updated." }
+      end
+    end
+  end
 
   def destroy
     @comment = @post.comments.find(params[:id])
