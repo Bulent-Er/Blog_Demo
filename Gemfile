a10c14f181927c42d5a920661a23f66b587eecac
Bülent ER <bropil@gmail.com>
2023-10-26 00:58:31 +0300

    insert performance eith ActiveRecord-import

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem "noticed", "~> 1.6"
 gem 'ransack', '~> 2.5'
 gem 'friendly_id', '~> 5.4.0'
 gem "bullet", "~> 7.1"
+
+gem "activerecord-import", "~> 1.5"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,44 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create(
-  email: "bropil@gmail.com",
-  name: "Bülent",
-  password: "123456", 
-  password_confirmation: "123456",
-  role: User.roles[:admin])
+            email: "bropil@gmail.com",
+            name: "Bülent",
+            password: "123456", 
+            password_confirmation: "123456",
+            role: User.roles[:admin])
 
 User.create(
-  email: "ezel@gmail.com",
-  name: "Ezel",
-  password: "123456", 
-  password_confirmation: "123456")
+            email: "ezel@gmail.com",
+            name: "Ezel",
+            password: "123456", 
+            password_confirmation: "123456")
 
-10.times do |x|
-  post = Post.create(
-    title: "Post #{x+1}", 
-    slug: nil,
-    body: Faker::Lorem.sentence(word_count: 10*(x+1)), 
-    user_id: x.odd? ? User.find(1).id : User.find(2).id)
-    
-    5.times do |y|
-      Comment.create(
-        post_id:  post.id,
-        body: Faker::Lorem.sentence(word_count: 5*(y+1)), 
-        user_id: y.even? ? User.find(1).id : User.find(2).id)
-    end
+posts = []
+comments = []
+elapsed = Benchmark.measure do
+  10.times do |x|
+    puts "Creating post #{x+1}"
+    post = Post.new(
+      title: "Post #{x+1}", 
+      slug: nil,
+      body: Faker::Lorem.sentence(word_count: 10*(x+1)), 
+      user_id: x.odd? ? User.find(1).id : User.find(2).id)
+      post.save!
+      posts << post
+      
+      5.times do |y|
+      puts "Creating comment #{y+1} for post #{x+1}"
+      comment = Comment.new(
+          post_id:  post.id,
+          body: Faker::Lorem.sentence(word_count: 5*(y+1)), 
+          user_id: y.even? ? User.find(1).id : User.find(2).id)
+          comment.save!
+          comments << comment
+      end
+  end
 end
+# Post.import(posts)
+# Comment.import(comments)
+puts "Done!"
 
-
+puts "Creating #{Post.count} posts and #{Comment.count} comments took #{elapsed.real} seconds"
